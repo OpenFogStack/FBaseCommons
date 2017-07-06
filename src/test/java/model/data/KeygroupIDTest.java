eaf0dbb7357a5f746bf6e476716c69a12ee3e11b
MoeweX <Jonathan@Hasenburg.de>
2017-07-06 11:06:08 +0200

    Rename KeygroupID test

--- src/test/java/model/data/KeygroupIDTest.java
+++ src/test/java/model/data/KeygroupIDTest.java
@@ -1,4 +1,4 @@
-package data.model;
+package model.data;
 
 import static org.junit.Assert.assertEquals;
 
@@ -10,7 +10,7 @@ import org.junit.Test;
 
 import model.data.KeygroupID;
 
-public class KeygroupTest {
+public class KeygroupIDTest {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
