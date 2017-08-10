0125378c36c3e8a0226e5499fc0d751db01eafb3
MoeweX <Jonathan@Hasenburg.de>
2017-08-11 00:28:02 +0200

    Add main method to RSAHelper for private key generation

--- src/main/java/crypto/RSAHelper.java
+++ src/main/java/crypto/RSAHelper.java
@@ -44,4 +44,11 @@ public class RSAHelper {
 		return Base64.getEncoder().encodeToString(key.getEncoded());
 	}
 
+	public static void main(String[] args) throws NoSuchAlgorithmException {
+		System.out.println("Printing keypair");
+		Pair<PublicKey, PrivateKey> keys = generateKeyPair(512);
+		System.out.println("Public: " + getEncodedStringFromKey(keys.getValue0()));
+		System.out.println("Private: " + getEncodedStringFromKey(keys.getValue1()));
+	}
+	
 }
